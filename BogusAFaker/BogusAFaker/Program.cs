83d6e9841a1d1bbab46f65b9f4b56f673e53f5d1
Subramanian, Ayyappan <asubramanian@HVHCVision.com>
2023-02-23 17:19:30 -0600

    Sample for bogus

---
+++ BogusAFaker/BogusAFaker/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+using Bogus;
+using Newtonsoft.Json;
+
+GenerateFakeEmployeeData();
+
+static void GenerateFakeEmployeeData()
+{
+    var departments = new [] { "Manufacturing", "Stores", "BackOffice", "Billing", "Insurance"};
+    var fakeEmployees = new Faker<Employee>();
+    fakeEmployees.RuleFor(x => x.FirstName, f => f.Name.FirstName());
+    fakeEmployees.RuleFor(x => x.LastName, f => f.Name.LastName());
+    fakeEmployees.RuleFor(x => x.EmployeeId, f => f.Random.Int(10000,20000));
+    fakeEmployees.RuleFor(x => x.Department, f => f.PickRandom(departments));
+    fakeEmployees.RuleFor(x => x.DateOfBirth, f => f.Date.Past(50).ToString("mm/dd/yyyy"));
+    var fakeEmployeeData= fakeEmployees.Generate(3);
+    Console.WriteLine(JsonConvert.SerializeObject(fakeEmployeeData));
+    Console.ReadLine();
+}
+
+
+public class Employee
+{
+    public int EmployeeId { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+
+    public string DateOfBirth { get; set; }
+
+    public string Department { get; set; }
+
+}
